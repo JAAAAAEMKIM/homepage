bf754c632bde0a3a50f7cfb4d911712b73ec2053
NHNkimjaemin <jaemin.kim@nhn.com>
2023-10-11 18:17:30 +0900

    index page eslint 오류 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,50 +1,43 @@
-import styles from "./page.module.css";
+import Link from 'next/link';
+import styles from './page.module.css';
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.heroText}>
-        I'M JAEMIN. <br /> I DEVELOP
+        {`I'M JAEMIN.`} <br /> I DEVELOP
         <br /> WEB <br />
         APPLICATIONS.
       </div>
-      <a
-        href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-        className={styles.card}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <h2>
-          About<span>-&gt;</span>
-        </h2>
-        <p>안녕하세요. 저는 </p>
-        프론트엔드 웹 개발자입니다.
-        <br />
-        현업에서는 React, Typescript를 주로 사용합니다. <br /> 개발 관련 공부를
-        좋아합니다. Email: jmkim326@gmail.com
-      </a>
-      <a
-        href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-        className={styles.card}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <h2>
-          Portfolio <span>-&gt;</span>
-        </h2>
-        <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-      </a>
-      <a
-        href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-        className={styles.card}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <h2>
-          Blog <span>-&gt;</span>
-        </h2>
-        <p>Explore the Next.js 13 playground.</p>
-      </a>
+      <div className={styles.contents}>
+        <a
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://blue-tang.tistory.com/"
+        >
+          <h2>
+            Blog <span>-&gt;</span>
+          </h2>
+          <p>Tistory 블로그</p>
+          <ul>
+            <li className={styles.card}>Post 1</li>
+            <li className={styles.card}>Post 2</li>
+            <li className={styles.card}>Post 3</li>
+          </ul>
+        </a>
+        <Link className={styles.card} href="/portfolio">
+          <h2>
+            Portfolio <span>-&gt;</span>
+          </h2>
+          <p>소개, 토이프로젝트 등</p>
+          <ul>
+            <li className={styles.card}>Project 1</li>
+            <li className={styles.card}>Project 2</li>
+            <li className={styles.card}>Project 3</li>
+          </ul>
+        </Link>
+      </div>
       Top
     </main>
   );
